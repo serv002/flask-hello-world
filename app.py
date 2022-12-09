19461261cbd0c0f315142b67e03aab2b09a9e849
serv002 <118726375+serv002@users.noreply.github.com>
2022-12-09 20:15:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from threading import Thread
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+def temp():
+    print("hi")
+
+if __name__ == "__main__":
+    t1 = Thread(target=temp)
+    t1.start()
+    app.run()
